07177e1f905ccb2f7048dc064065e2b2eb3029a5
Antoine Cusset <a.cusset@gmail.com>
2016-10-19 10:25:11 +0000

    __get __set function user

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -94,5 +94,15 @@ class User extends BaseUser
         return $this->lastName;
     }
 
+    function __get($name)
+    {
+        return $this->$name;
+    }
+
+    function __set($name, $value)
+    {
+        $this->$name($value);
+    }
+
 }
 
