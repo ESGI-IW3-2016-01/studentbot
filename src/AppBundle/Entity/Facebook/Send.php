a3f5a4c48ca74dbe866d427da7b8d22a12e6a935
Antoine Cusset <a.cusset@gmail.com>
2016-10-31 10:38:50 +0000

    just trolling the repo

---
+++ src/AppBundle/Entity/Facebook/Send.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Antoine
+ * Date: 30/10/2016
+ * Time: 10:28
+ */
+class Send
+{
+    private $recipient;
+    private $message;
+    private $senderAction;
+    private $notificationType;
+
+}
