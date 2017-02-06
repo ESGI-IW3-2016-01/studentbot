6584593fedb0fe46fffd62cceaac883dba5eeb6c
Antoine <a.cusset@gmail.com>
2017-02-06 23:16:33 +0100

    Api repositories

---
+++ src/AppBundle/Repository/ApiLogRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: antoine
+ * Date: 06/02/2017
+ * Time: 23:15
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class ApiLogRepository extends EntityRepository
+{
+
+}
