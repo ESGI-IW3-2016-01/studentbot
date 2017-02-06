56e497b00cb9ece5cce496dc8d4f39400bd1b9f9
Alexandre Morin <alexandre.morin@maarch.org>
2017-02-06 18:04:55 +0100

    Service weather

---
+++ src/AppBundle/Service/Weather.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Service;
+
+class Weather
+{
+    private $lang;
+    private $unity;
+    
+    public function __construct($lang = "fr", $unity = "metric")
+    {
+        $this->lang = $lang;
+        $this->unity = $unity;
+        $this->client = new Client(['base_uri' => 'http://api.openweathermap.org/data/2.5/weather']);
+    }
+    
+    public function getWeatherByCity($city)
+    {
+        $uri = "?q=" . $city . "&lang=" . $this->lang . "&unity =" . $this->unity;
+        
+        try {
+            $response = $this->client->get($uri);
+            error_log('[Guzzle Response] ' . $response->getStatusCode() . ' : ' . $response->getBody());
+            return $response;
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+    
+    public function getWeatherByGeographicCoordinates($lat, $lon)
+    {
+        $uri = "?lat=" . $lat . "&lon =" . $lon . "&lang=" . $this->lang . "&unity =" . $this->unity;
+        
+        try {
+            $response = $this->client->get($uri);
+            error_log('[Guzzle Response] ' . $response->getStatusCode() . ' : ' . $response->getBody());
+            return $response;
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+}
+
