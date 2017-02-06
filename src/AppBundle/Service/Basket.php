cc9e775a2a17380d1afa6c2582fa54f1bda8f7ab
Alexandre Morin <alexandre.morin@maarch.org>
2017-02-06 17:10:53 +0100

    Service sport

---
+++ src/AppBundle/Service/Basket.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Service;
+
+class Basket
+{   
+    public function __construct()
+    {
+        $this->client = new Client(['base_uri' => 'http://api.sportradar.us/nba-t3/']);
+    }
+    
+    public function getResultNBA() 
+    {
+        $date = date("Y/m/d");
+        
+        $uri = "games/" . $date . "/schedule.json?api_key=" . BASKET_KEY;
+            
+        try {
+            $response = $this->client->get($uri);
+            error_log('[Guzzle Response] ' . $response->getStatusCode() . ' : ' . $response->getBody());
+            return $response;
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+}
---
+++ src/AppBundle/Service/Football.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Service;
+
+class Football
+{   
+    public function __construct()
+    {
+        $this->client = new Client(['base_uri' => 'https://api.sportradar.us/soccer-t3/eu/fr/']);
+    }
+    
+    public function getResultFootball() 
+    {
+        $date = date ("Y-m-d", mktime (0,0,0,date('m'),date('d')-1,date('Y')));
+        $uri = "schedules/" . $date . "/results.json?api_key=" . FOOTBALL_KEY;
+         
+        try {
+            $response = $this->client->get($uri);
+            error_log('[Guzzle Response] ' . $response->getStatusCode() . ' : ' . $response->getBody());
+            return $response;
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+}
