7893bbc2edf1f882fde21ed8d47e1311780e9a98
Alexandre Morin <alexandre.morin@maarch.org>
2017-02-08 09:37:32 +0100

    Service basket

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -111,8 +111,56 @@ class DefaultController extends Controller
             case "\xE2\x9A\xBD" :
                 $res =$this->football();
                 break;
+            case "\xF0\x9F\x8F\x80" :
+                $res = $this->basket();
+                break;
             default :
                 $res = $chaine;
+        }
+        
+        return $res;
+    }
+    
+    private function basket() {
+        $basket = new Basket();
+        $json_data = $basket->getResultNBA();
+        
+        $data = json_decode($json_data);
+        
+        $res = [];
+         foreach ($data->games as $games) {
+            $home_team = $games->home->name;
+            $away_team = $games->away->name;
+            $status = $status = $this->getStatusNBA($games->status);
+            
+            
+            if ($games->status == "closed") {
+                $home_points = $games->home_points;
+                $away_points = $games->away_points;
+                
+                $res[] = $status. " - ". $away_team." ".$away_points. " - ".$home_points. " " .$home_team; 
+            } else {
+                $res[] = $status. " - ". $away_team. " vs " .$home_team; 
+            }
+        }
+        
+        return $res;
+    }
+    
+    private function getStatusNBA($status) {
+        switch ($status) {
+            case 'closed':
+                $res = "Closed \xf0\x9f\x94\x92";
+                break;
+            case 'scheduled':
+                $res = "Scheduled \xf0\x9f\x93\x85";
+                break;
+            case 'in progress':
+                $res = "In progress \xe2\x8f\xb3";
+                break;
+            default:
+                $res = $status;
+                break;
         }
         
         return $res;
