730f1f6fb76ba1a3e5af5c63218cdc53c24b29ac
Alexandre Morin <morinalexandrem@gmail.com>
2017-02-08 07:51:51 +0000

    Resolved merge conflict by incorporating both suggestions.

--- src/AppBundle/Service/Football.php
+++ src/AppBundle/Service/Football.php
@@ -4,13 +4,12 @@ namespace AppBundle\Service;
 
 use GuzzleHttp\Client;
 
-class Football extends ContainerAwareCommand
+class Football
 {   
     private $football_key;
     public function __construct()
     {
-        $container = $this->getContainer();
-        $this->football_key = $container->getParameter('football_key');
+        $this->football_key = "ff7hfsyugcsx777g8j56qzrq";
         $this->client = new Client(['base_uri' => 'https://api.sportradar.us/soccer-t3/eu/fr/']);
     }
     
@@ -19,6 +18,7 @@ class Football extends ContainerAwareCommand
         $date = date ("Y-m-d", mktime (0,0,0,date('m'),date('d')-1,date('Y')));
         $uri = "schedules/" . $date . "/results.json?api_key=" . $this->football_key;
          
+         error_log($uri);
         try {
             $response = $this->client->get($uri);
             error_log('[Guzzle Response] ' . $response->getStatusCode() . ' : ' . $response->getBody());
