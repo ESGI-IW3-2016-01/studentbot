3a0473a0b10f3c8398837b07fab3233dc12636c4
Alexandre Morin <alexandre.morin@maarch.org>
2017-02-08 08:49:21 +0100

    Trait

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,7 @@ use AppBundle\Entity\Facebook\Message;
 use AppBundle\Service\Basket;
 use AppBundle\Service\Football;
 use AppBundle\Service\Weather;
+use AppBundle\Controller\footballControllerTrait;
 
 class DefaultController extends Controller
 {
@@ -116,68 +117,5 @@ class DefaultController extends Controller
         }
         
         return $res;
-    }
-    
-    private function football() {
-        $football = new Football();
-        $json_data = $football->getResultFootball();
-        
-        $data = json_decode($json_data);
-        
-        $res = [];
-        foreach ($data->results as $result) {
-            $home_team = $result->sport_event->competitors[0]->name;
-            $away_team = $result->sport_event->competitors[1]->name;
-            $home_score = $result->sport_event_status->home_score;
-            $away_score = $result->sport_event_status->away_score;
-            $tournament = $result->sport_event->tournament->name;
-
-            $flag = $this->getTournamentFlag($tournament);
-            
-            $str = $tournament. " " . $flag . " - " . $home_team." ".$home_score. " - ".$away_score." ".$away_team;
-            $res[] = $str;
-        }
-        
-        return $res;
-    }
-    
-    private function getTournamentFlag($codeFlag) {
-        switch ($codeFlag) {
-            case 'LaLiga Santander':
-                $flag = "\xF0\x9F\x87\xAA\xF0\x9F\x87\xB8";
-                break;
-            case 'Eredivisie':
-                $flag = "\xf0\x9f\x87\xb3\xf0\x9f\x87\xb1";
-                break;
-            case 'Serie A':
-                $flag = "\xF0\x9F\x87\xAE\xF0\x9F\x87\xB9";
-                break;
-            case 'Super League':
-                $flag = "\xf0\x9f\x87\xac\xf0\x9f\x87\xb7";
-                break;
-            case 'Premier League':
-                $flag = "\xF0\x9F\x87\xAC\xF0\x9F\x87\xA7";
-                break;
-            case 'Division 1A':
-                $flag = "\xf0\x9f\x87\xa7\xf0\x9f\x87\xaa";
-                break;
-            case 'Serie B':
-                $flag = "\xF0\x9F\x87\xAE\xF0\x9F\x87\xB9";
-                break;
-            case 'Bundesliga':
-                $flag = "\xF0\x9F\x87\xA9\xF0\x9F\x87\xAA";
-                break;
-            case 'Primeira Liga':
-                $flag = "\xf0\x9f\x87\xb5\xf0\x9f\x87\xb9";
-                break;
-            case 'Ligue 1':
-                $flag = "\xF0\x9F\x87\xAB\xF0\x9F\x87\xB7";
-                break;
-            default:
-                $flag = " ";
-                break;
-        }
-        
-        return $flag;
     }
 }
