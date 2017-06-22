52c85008afe73246698821739103ba82c184fce0
Antoine Cusset <acusset@clever-age.com>
2017-06-22 16:34:45 +0200

    get facebook admins after install script

--- src/AppBundle/Command/AdminFbCommand.php
+++ src/AppBundle/Command/AdminFbCommand.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Repository\UserRepository;
 use Doctrine\ORM\EntityManager;
+use Facebook\FacebookResponse;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,37 +13,60 @@ use AppBundle\Entity\User;
 
 class AdminFbCommand extends ContainerAwareCommand
 {
+    /**
+     * @var string
+     */
+    private $fbSecret;
+
+    /**
+     * @var string
+     */
+    private $fbAppId;
+
+    /**
+     * @var Facebook
+     */
+    private $fb;
+
     protected function configure()
     {
         $this
-            ->setName('admin:fb')
-            ->setDescription("add to database administrators fb of student app");
+            ->setName('facebook:get:admin')
+            ->setDescription("Get Facebook Application administrators and populate database");
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->fbAppId = $this->getContainer()->getParameter('facebook.app_id');
+        $this->fbSecret = $this->getContainer()->getParameter('facebook.app_secret');
+
+        $this->fb = new Facebook([
+            'app_id' => $this->fbAppId,
+            'app_secret' => $this->fbSecret,
+        ]);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var EntityManager $em */
         $em = $this->getContainer()->get('Doctrine')->getManager();
+
+        /** @var UserRepository $userRepository */
         $userRepository = $em->getRepository('AppBundle\Entity\User');
 
-        $fbAppId = $this->getContainer()->getParameter('facebook.app_id');
-        $fbSecret = $this->getContainer()->getParameter('facebook.app_secret');
+        /** @var FacebookResponse $requestRoles */
+        $requestRoles = $this->fb->get("$this->fbAppId/roles", "$this->fbAppId|$this->fbSecret");
 
-        $fb = new Facebook([
-            'app_id' => $fbAppId,
-            'app_secret' => $fbSecret,
-        ]);
-
-        $requestRoles = $fb->get($fbAppId."/roles", $fbAppId.'|'.$fbSecret);
         // on récupère tous les users de l'application ainsi que leur rôle au sein de l'appli
         $roles = $requestRoles->getDecodedBody()['data'];
         $i = 0;
 
-        foreach($roles as $key => $value){
-            if ($value["role"] == "administrators"){
-                $myAdminCall = $fb->get($value['user'].'?fields=email,name,first_name,last_name', $fbAppId.'|'.$fbSecret);
+        foreach ($roles as $key => $value) {
+            if ($value["role"] == "administrators") {
+                $myAdminCall = $this->fb->get($value['user'] . '?fields=email,name,first_name,last_name', $this->fbAppId . '|' . $this->fbSecret);
                 $myAdminData = $myAdminCall->getDecodedBody();
                 $email = (isset($myAdminData['email'])) ? $myAdminData['email'] : '';
-                
+
                 $existUser = $userRepository->findOneByEmail($email);
 
                 if (!$existUser && !empty($email)) {
@@ -57,8 +82,7 @@ class AdminFbCommand extends ContainerAwareCommand
                     $em->persist($user);
                     $em->flush();
                     $i++;
-                }
-                elseif ($existUser) {
+                } elseif ($existUser) {
                     if ($existUser->hasRole('ROLE_SUPER_ADMIN') == false) {
                         $existUser->setRoles(['ROLE_SUPER_ADMIN']);
                         $em->flush();
@@ -67,6 +91,6 @@ class AdminFbCommand extends ContainerAwareCommand
             }
         }
 
-        $output->writeln($i." Administrateurs ajoutés.");
+        $output->writeln($i . " Administrateurs ajoutés.");
     }
 }
