3d2ae025a6d8ce1c9dc9d1da6e88954cb32a58ce
Alexandre Morin <alexandre.morin@maarch.org>
2017-06-28 15:42:28 +0200

    close #5
    Upload Calendar file to app (local or aws s3), retrieve and parse command
    Entity Calendar, Group and School fix and improvements
    Cherry picked refs #2 commit

---
+++ src/AppBundle/Entity/School/Promotion.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace AppBundle\Entity\School;
+
+use Doctrine\ORM\Mapping as ORM;
+use DateTime;
+
+/**
+ * Promotion
+ *
+ * @ORM\Table(name="promotion")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PromotionRepository")
+ */
+class Promotion
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="start_date", type="datetime")
+     */
+    private $startDate;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="end_date", type="datetime")
+     */
+    private $endDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set startDate
+     *
+     * @param DateTime $startDate
+     *
+     * @return Promotion
+     */
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    /**
+     * Get startDate
+     *
+     * @return DateTime
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * Set endDate
+     *
+     * @param DateTime $endDate
+     *
+     * @return Promotion
+     */
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    /**
+     * Get endDate
+     *
+     * @return DateTime
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    public function __toString()
+    {
+        return $this->startDate->format('d/m/Y').'-'.$this->endDate->format('d/m/Y');
+    }
+}
+
