04d2df445c6b08d71b39136675a1fa9ec7abe348
Alexandre Morin <alexandre.morin@maarch.org>
2017-07-06 21:40:26 +0200

    Disable/Enable API

---
+++ src/AppBundle/Service/ApiService.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Service;
+use AppBundle\Entity\Api;
+use Doctrine\ORM\EntityManager;
+
+class ApiService
+{
+    /** @var EntityManager $manager */
+    private $manager;
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->manager = $entityManager;
+    }
+
+    public function getApi($apiName) {
+        $apiRepository = $this->manager->getRepository('AppBundle:Api');
+        $api = $apiRepository->findOneBy(["name" => $apiName]);
+        $this->manager->flush();
+
+        return $api;
+    }
+}
