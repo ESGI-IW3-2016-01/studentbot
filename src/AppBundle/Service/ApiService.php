f2d130d441a1cb5e7b6a070e6ec8c3fafde5ac08
Antoine Cusset <acusset@clever-age.com>
2017-07-08 16:40:37 +0200

    I hate this fucking language.

--- src/AppBundle/Service/ApiService.php
+++ src/AppBundle/Service/ApiService.php
@@ -1,18 +1,28 @@
 <?php
+
 namespace AppBundle\Service;
+
 use AppBundle\Entity\Api;
 use Doctrine\ORM\EntityManager;
 
 class ApiService
 {
-    /** @var EntityManager $manager */
+    /**
+     * @var EntityManager $manager
+     */
     private $manager;
+
+    /**
+     * ApiService constructor.
+     * @param EntityManager $entityManager
+     */
     public function __construct(EntityManager $entityManager)
     {
         $this->manager = $entityManager;
     }
 
-    public function getApi($apiName) {
+    public function getApi($apiName)
+    {
         $apiRepository = $this->manager->getRepository('AppBundle:Api');
         $api = $apiRepository->findOneBy(["name" => $apiName, "enabled" => true]);
         $this->manager->flush();
