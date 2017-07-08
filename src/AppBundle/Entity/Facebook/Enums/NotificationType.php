c2b9aad3810ef14c8af97fff19e888ba4768459b
Antoine Cusset <acusset@clever-age.com>
2017-07-08 16:34:23 +0200

    I hate this fucking language.

--- src/AppBundle/Entity/Facebook/Enums/NotificationType.php
+++ src/AppBundle/Entity/Facebook/Enums/NotificationType.php
@@ -1,15 +1,23 @@
 <?php
 
 
+/**
+ * Class NotificationType
+ *
+ * @author Antoine Cusset <a.cusset@gmail.com>
+ * @link https://github.com/acusset
+ * @category
+ * @license
+ */
 abstract class NotificationType
 {
-    const regular = "REGULAR";
-    const silentPush = "SILENT_PUSH";
-    const noPush = "NO_PUSH";
+    const NOTIFICATION_REGULAR = 'REGULAR';
+    const NOTIFICATION_SILENT_PUSH = 'SILENT_PUSH';
+    const NOTIFICATION_NO_PUSH = 'NO_PUSH';
 
-//    const codes = [
-//        self::markSeen,
-//        self::typingOn,
-//        self::typingOff
-//    ];
+    const TYPES = [
+        self::NOTIFICATION_REGULAR,
+        self::NOTIFICATION_SILENT_PUSH,
+        self::NOTIFICATION_NO_PUSH,
+    ];
 }
