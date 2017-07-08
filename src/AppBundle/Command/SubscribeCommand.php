5ec70e6c61836dba85be04a72a1fac965d4dd138
Antoine Cusset <acusset@clever-age.com>
2017-07-08 16:31:27 +0200

    Enhance subscribe command

--- src/AppBundle/Command/SubscribeCommand.php
+++ src/AppBundle/Command/SubscribeCommand.php
@@ -3,16 +3,37 @@
 namespace AppBundle\Command;
 
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+/**
+ * Class SubscribeCommand
+ *
+ * @author Antoine Cusset <a.cusset@gmail.com>
+ * @link https://github.com/acusset
+ * @category
+ * @license
+ * @package AppBundle\Command
+ */
 class SubscribeCommand extends ContainerAwareCommand
 {
 
+    /**
+     * @var string $graphVersion
+     */
     protected $graphVersion;
+
+    /**
+     * @var string $accessToken
+     */
     protected $accessToken;
+
+    /**
+     * @var Client $client
+     */
     protected $client;
 
     protected function configure()
@@ -22,6 +43,10 @@ class SubscribeCommand extends ContainerAwareCommand
             ->setDescription('Send POST request to facebook API to enable webhook');
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
         $container = $this->getContainer();
@@ -32,6 +57,11 @@ class SubscribeCommand extends ContainerAwareCommand
         $this->client = new Client(['base_uri' => 'https://graph.facebook.com/']);
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return null
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $uri = $this->graphVersion . '/me/subscribed_apps';
@@ -40,13 +70,23 @@ class SubscribeCommand extends ContainerAwareCommand
         $io->title('Sending request to Facebook...');
 
         try {
-            $response = $this->client->post($uri, ['query' => ['access_token' => $this->accessToken]]);
+            /** @var ResponseInterface $response */
+            $response = $this->client->post(
+                $uri,
+                [
+                    'query' => [
+                        'access_token' => $this->accessToken
+                    ]
+                ]
+            );
         } catch (\Exception $e) {
             error_log($e->getMessage());
-            return $io->error(sprintf('Error : %s', $e->getMessage()));
+            $io->error(sprintf('Error : %s', $e->getMessage()));
         }
 
         $io->text('[Response] ' . $response->getStatusCode() . ' : ' . $response->getBody());
         $io->success('Webhook subscribed to the page events');
+
+        return null;
     }
 }
