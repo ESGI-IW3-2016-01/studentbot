779684626d5c30024a51192df40d74f35797a6c4
Antoine Cusset <acusset@clever-age.com>
2017-07-09 23:34:44 +0200

    typo

--- src/AppBundle/Service/UserService.php
+++ src/AppBundle/Service/UserService.php
@@ -72,7 +72,7 @@ class UserService
             $username = $userData['first_name'] . ' ' . $userData['last_name'];
 
             $user = $this->repository
-                ->findOneBy(['username_canonical' => $senderId]);
+                ->findOneBy(['usernameCanonical' => $senderId]);
 
             if (!$user) {
                 $user = new User();
