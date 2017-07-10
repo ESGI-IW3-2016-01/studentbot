077db353f31c1bccaf4695b5a6337b872d8dba9a
Jolan Levy <jolan.levy@gmail.com>
2017-07-10 16:47:30 +0200

    Fix bug get event suite ...

--- src/AppBundle/Service/Calendar.php
+++ src/AppBundle/Service/Calendar.php
@@ -45,12 +45,11 @@ class Calendar
         $next_class = $eventRepository->findNextClass($calendar->getId());
 
         if (empty($next_class)){
-            return "Vous n'avez aucun prochain cours";
+            return "Vous n'avez aucun prochain cours aujourd'hui";
+        } else {
+            $res =  $this->eventString($next_class);
+            return $res;
         }
-        if (is_array($next_class)) {
-            return $next_class[0];
-        }
-        return $next_class;
         
     }
 
@@ -77,10 +76,20 @@ class Calendar
         }
         $day_class = $eventRepository->findDayClass($calendar->getId());
 
+
         if (empty($day_class)){
-            return "Vous n'avez aucun prochain cours";
+            return "Vous n'avez plus de cours aujourd'hui";
+        } else {
+            if (is_array($day_class)){
+                $res = "";
+                foreach ($day_class as $event) {
+                    $res .= $this->eventString($event);
+                }
+            } else {
+                $res =  $this->eventString($day_class);
+            }
+            return $res;
         }
-        return $day_class;
     }
 
     public function getWeekClass($current_user)
@@ -106,9 +115,31 @@ class Calendar
         }
         $week_class = $eventRepository->findWeekClass($calendar->getId());
 
+
         if (empty($week_class)){
-            return "Vous n'avez aucun prochain cours";
+            return "Vous n'avez plus de cours cette semaine";
+        } else {
+            if (is_array($week_class)){
+                $res = "";
+                foreach ($week_class as $event) {
+                    $res .= $this->eventString($event);
+                }
+            } else {
+                $res =  $this->eventString($week_class);
+            }
+            return $res;
         }
-        return $week_class;
+    }
+
+    private function eventString($event){
+        if (is_array($event)) {
+            $event = $event[0];
+        }
+
+        $res = (string)$event->getSummary()."\x0D\x0A\\";
+        $res .= "De : ".date_format($event->getStartAt(), 'H:i').". A : ".date_format($event->getEndAt(), 'H:i')."."."\x0D\x0A\\";
+        $res .= (string)$event->getDescription()."\x0D\x0A\\";
+
+        return $res;
     }
 }
